628fc701bc1f0433b689709830285bc4015e80bb
gabrielelicea <48261085+gabrielelicea@users.noreply.github.com>
2020-09-25 14:10:04 -0700

    Update Hernandez_Assignment3.py

--- Hernandez_Assignment3.py
+++ Hernandez_Assignment3.py
@@ -1,9 +1,19 @@
 import requests
 import csv
 import operator
+import re
+
+
+def listMerger(link):
+    resp = requests.get(link)
+    file = resp.text
+    lines = file.splitlines()
+    for line in lines:
+        mergedList.append(line)
+
 
 quit = False
-while quit != True:
+while not quit:
     print("        Hernandez's CompanyList Data Analyzer: ")
     print("========================================================")
     print("1. Export to merged/sorted(by stock symbol) CSV file")
@@ -20,41 +30,34 @@ while quit != True:
     if option == 1:
         # categories = "Symbol Name LastSale MarketCap ADR TSO IPO year Sector	Industry Summary Quote"
         mergedList = []
-        resp1 = requests.get(link1)
-        file1 = resp1.text
-        lines1 = file1.splitlines()
-        for line in lines1:
-            mergedList.append(line)
 
-        resp2 = requests.get(link2)
-        file2 = resp2.text
-        lines2 = file2.splitlines()
-        for line in lines2:
-            mergedList.append(line)
+        listMerger(link1)
+        listMerger(link2)
+        listMerger(link3)
 
-        resp3 = requests.get(link3)
-        file3 = resp3.text
-        lines3 = file3.splitlines()
-        for line in lines3:
-            mergedList.append(line)
-            mergedList.sort()
-
+        mergedList.sort()
         f = open("mergedCompanyList.csv", "w")
         f.writelines('\n'.join(mergedList))
         f.close()
 
     elif option == 2:
         # search for stock
-        f1 = open('mergedCompanyList.csv', 'r')
-        f1.readline()
-        # f2 = sorted(f1,key=operator.itemgetter(5),reverse=True)
-        # csv1 = csv.reader(f1, delimiter=',')
-        # sort = sorted(csv1, key=operator.itemgetter(2))
-        # for eachline in f2:
-        #     print(eachline)
-    # elif option == 3:
-    # display 15 companies with highest market cap
+        stockSearch = input("Enter the stock symbol you wish to search: ")
+        with open('mergedCompanyList.csv', 'r') as f:
+            for line in f:
+                if stockSearch in line:
+                    print(line)
+                    break
+
+    elif option == 3:
+        reader = csv.reader(open("mergedCompanyList.csv"), re=',')
+        for Symbol, Name, LastSale, MarketCap, ADR, TSO, IPOyear, Sector, Industry, SummaryQuote, in reader:
+            sortedList = sorted(reader, key=operator.itemgetter(2), reverse=True)
+            print(sortedList)
+            # display 15 companies with highest market cap
+
     elif option == 4:
         quit = True
     else:
         raise Exception("Wrong Option")
+
